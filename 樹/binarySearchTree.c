3a0a025f9ab3cad94b184fc44e890cba52ec3d7a
kai2june <104703027@nccu.edu.tw>
2018-09-16 09:51:50 +0800

    levelorder finished.

--- 樹/binarySearchTree.c
+++ 樹/binarySearchTree.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 
 typedef struct treeNode {
     int num;
@@ -17,6 +16,11 @@ TreeNode* createTreeNode(int num){
 }
 
 void insertTreeNode(TreeNode** currentNode, TreeNode** newNode){
+    if( (*currentNode) == NULL ){
+        (*currentNode) = (*newNode);
+        return;
+    }
+
     if( (*newNode) -> num <= (*currentNode) -> num ){
         if( (*currentNode) -> leftChild == NULL )
             (*currentNode) -> leftChild = (*newNode);
@@ -55,24 +59,42 @@ void postorderTraversal(TreeNode** currentNode){
     printf("%d ", (*currentNode) -> num);
 }
 
+void levelorderTraversal(TreeNode* treeNodeQueue[], int top, int last){
+    if( top == last + 1 && top != 0 || treeNodeQueue[0] == NULL)
+        return;  
+    if( treeNodeQueue[top] -> leftChild )
+        treeNodeQueue[++last] = treeNodeQueue[top] -> leftChild;
+    if( treeNodeQueue[top] -> rightChild )
+        treeNodeQueue[++last] = treeNodeQueue[top] -> rightChild;
+    printf("%d ", treeNodeQueue[top++] -> num);
+    levelorderTraversal(treeNodeQueue, top, last);
+}
+
 int main(int argc, char* argv[]){
 
     int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
     TreeNode* root = NULL;
-    if(sizeof(arr) > 0)
-        root = createTreeNode(arr[0]);
     TreeNode* newNode = NULL;
-    int i = 1;
+    TreeNode* treeNodeQueue[sizeof(arr)/sizeof(*arr)];
+    
+    int i = 0, j = 0;
     for(; i<sizeof(arr)/sizeof(*arr); i++){
         newNode = createTreeNode(arr[i]);
         insertTreeNode(&root, &newNode);
+        treeNodeQueue[i] = NULL;
     }
+    if( root )
+        treeNodeQueue[0] = root;
+    
     printf("preorderTraversal:\n");
     preorderTraversal(&root);
     printf("\ninorderTraversal:\n");
     inorderTraversal(&root);
     printf("\npostorderTraversal:\n");
     postorderTraversal(&root);
+    printf("\nlevelorderTraversal:\n");
+    i=0;
+    levelorderTraversal(treeNodeQueue, i, j);
 
     return 0;
 }
